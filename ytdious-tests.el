9eb2313c1dc27b8da6bc2367aff34b61048361f3
Štěpán Němec <stepnem@gmail.com>
2022-08-04 20:56:11 +0200

    Fix the ERT test(s)
    - add missing require
    - update library prefix
    - turn off checkdoc
    - delete obsolete test

--- ytdious-tests.el
+++ ytdious-tests.el
@@ -1,25 +1,21 @@
-;;; The tests
-
 (require 'ert)
+(require 'ytdious)
 
-(ert-deftest ytel--format-author-test ()
-  "Test the `format-author' function."
-  (should (equal (length (ytel--format-author "channel name"))
-		 (+ 3 ytel-author-name-reserved-space)))
-  (should (equal (length (ytel--format-author "very very long channel name"))
-		 (+ 3 ytel-author-name-reserved-space))))
-
-(ert-deftest ytel--format-video-test ()
-  "test the `format-video' test."
-  (should (equal (ytel--format-video-length 60)
+(ert-deftest ytdious--format-video-length-test ()
+  "Test `ytdious--format-video-length'."
+  (should (equal (ytdious--format-video-length 60)
 		 "00:01:00"))
-  (should (equal (ytel--format-video-length 72)
+  (should (equal (ytdious--format-video-length 72)
 		 "00:01:12"))
-  (should (equal (ytel--format-video-length 134)
+  (should (equal (ytdious--format-video-length 134)
 		 "00:02:14"))
-  (should (equal (ytel--format-video-length 3600)
+  (should (equal (ytdious--format-video-length 3600)
 		 "01:00:00"))
-  (should (equal (ytel--format-video-length 5100)
+  (should (equal (ytdious--format-video-length 5100)
 		 "01:25:00"))
-  (should (equal (ytel--format-video-length 5430)
+  (should (equal (ytdious--format-video-length 5430)
 		 "01:30:30")))
+
+;; Local Variables:
+;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
+;; End:
