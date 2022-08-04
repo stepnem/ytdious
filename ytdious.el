stepnem/ytdious

commit d44de885c0001ea1ac6e3d0177db2479f75e1c72

ytdious--API-call: fix logic, save resources by specifying "fields"
(logic: don't accept both UCID and video query parameters)

ytdious.el
@@ -445,16 +445,21 @@ exits with a non-zero exit code, otherwise the request body is
 parsed by `json-read' and returned.  Optional argument UCID specifies
 the channel to search."
   (with-temp-buffer
-    (let ((exit-code
-           (call-process
-            "curl" nil t nil
-            "--silent"
-            "-X" "GET"
-            (concat ytdious-invidious-api-url
-                    "/api/v1/"
-                    method
-                    (when ucid (format "/%s%s" ucid "?sort_by=newest"))
-                    (when args (concat "?" (url-build-query-string args)))))))
+    (let* ((fields-param
+	    "fields=author,lengthSeconds,published,title,viewCount,videoId")
+	   (exit-code
+            (call-process
+             "curl" nil t nil
+             "--silent"
+             "-X" "GET"
+             (concat
+	      ytdious-invidious-api-url
+              "/api/v1/"
+              method
+              (if ucid (concat "/" ucid "?sort_by=newest&" fields-param)
+                (concat "?" fields-param
+			(when args
+			  (concat "&" (url-build-query-string args)))))))))
       (unless (= exit-code 0)
         (error "Curl exited with code %d when querying Invidious" exit-code))
       (goto-char (point-min))

commit 550bc2c41dd1e4a209af64e0c619875305fd7369

ytdious-invidious-default-query-fields: refactor
Rename to ytdious-fields, make it affect both video and channel
searches.

Fixes: d44de885c000 ("ytdious--API-call: fix logic, save resources by specifying "fields"")

ytdious.el
@@ -48,9 +48,9 @@ The instance must support the Invidious API.  A list of
 available instances can be found at the URL
 `https://api.invidious.io/?sort_by=api'.")
 
-(defvar ytdious-invidious-default-query-fields
-  "author,lengthSeconds,title,videoId,authorId,viewCount,published"
-  "Default fields of interest for video search.")
+(defvar ytdious-fields
+  "author,authorID,lengthSeconds,published,title,videoId,viewCount"
+  "Fields of interest for video search.")
 
 (defvar-local ytdious-videos nil
   "List of videos currently on display.")
@@ -274,8 +274,7 @@ OFFLINE means don't query the API, just redraw the list."
    `(("q" ,string)
      ("date" ,ytdious-date-criterion)
      ("sort_by" ,ytdious-sort-criterion)
-     ("page" ,ytdious-current-page)
-     ("fields" ,ytdious-invidious-default-query-fields))))
+     ("page" ,ytdious-current-page))))
 
 (defun ytdious--query-channel (string)
   "Show YouTube channel STRING."
@@ -417,8 +416,7 @@ exits with a non-zero exit code, otherwise the request body is
 parsed by `json-read' and returned.  Optional argument UCID specifies
 the channel to search."
   (with-temp-buffer
-    (let* ((fields-param
-	    "fields=author,lengthSeconds,published,title,viewCount,videoId")
+    (let* ((fields-param (concat "fields=" ytdious-fields))
 	   (exit-code
             (call-process
              "curl" nil t nil